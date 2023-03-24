ac9348cb47da9893f7efaa9d7b30d1ec2f4ddc57
Niklas Kleiser <nk-213780@rwu.de>
2023-03-24 15:23:48 +0100

    added item endpoints and image endpoints

--- index.js
+++ index.js
@@ -2,20 +2,12 @@
 const express = require('express');
 const cors = require('cors')
 const pkg = require('./package.json');
+const fs = require('fs')
 
 // Configure routes
 const router = express.Router();
 // App constants
 const port = process.env.PORT || 3000;
-
-// Store data in-memory, not suited for production use!
-const db = {
-  productCatalog: [
-    { name: "Krumbach Wasserflasche", pictureLoc: "src/assets/krumbach.jpg", barcode: 4009228120053 },
-    { name: "Aqua Wasserflasche", pictureLoc: "src/assets/aqua.jpg", barcode: 4001428065417 },
-    { name: "Kreidebox", pictureLoc: "src/assets/krumbach.jpg", barcode: 12134 },
-  ]
-  };
   
 // Create the Express app & setup middlewares
 const app = express();
@@ -25,13 +17,45 @@ app.options('*', cors());
 
 
 
-
 // Hello World for index page
-app.get('/', function (req, res) {
-    return res.json(db.productCatalog);
+app.get('/api/item/:id', function (req, res) {
+    const id = req.params.id;
+    fs.readFile('./db.txt','utf-8',(err,content)=>{
+      if(err) throw err;
+      res.writeHead(200)
+      const item = JSON.parse(content).filter(product => product.barcode = id)[0]
+      res.write(JSON.stringify(item))
+      return res.end()
+  })
+    
 })
 
+app.get('/api/items', function (req, res) {
+  const id = req.params.id;
+  fs.readFile('./db.txt','utf-8',(err,content)=>{
+    if(err) throw err;
+    res.writeHead(200)
+    res.write(JSON.stringify(content))
+    return res.end()
+})
   
+})
+
+app.get('/api/image/:name', (req, res) => {
+  const name = req.params.name
+  console.log(name)
+  if (name !== undefined && name !== 'undefined') {
+    res.sendFile('/assets/' + name, { root: __dirname }, function (err) {
+      if (err) {
+        res
+          .status(500)
+          .send('Oh uh, something went wrong on tha server. ' + err)
+      }
+    })
+  }
+})
+
+
 
 // Start the server
 app.listen(port, () => {
